e34eef50b060c9e86c65604c6811ffba294e4bdc
Tobias Sartori <tobiasfaculdade@gmail.com>
2025-02-15 19:42:22 -0300

    airflow setup

---
+++ dags/news/get_data_news_api.py
@@ -0,0 +1,37 @@
+from airflow.decorators import dag
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import datetime, timedelta
+import os
+from airflow.models import Variable
+from include.python_operators.news.get_data_news_api import get_data_news_api, TestPostgresConnectionOperator
+
+
+@dag(
+    description="This dag gets data from the news API",
+    default_args={
+        "owner": "Tobias Sartori",
+        "start_date": datetime(2025, 1, 9),
+        "retries": 0,
+        "execution_timeout": timedelta(hours=1),
+    },
+    start_date=datetime(2025, 2, 10),
+    max_active_runs=1,
+    schedule_interval="@daily",
+    catchup=True,
+    tags=["news"],
+)
+
+def _get_data_news_api():
+
+    test_postgres_connection_operator = TestPostgresConnectionOperator(
+        task_id="test_postgres_connection",
+    )
+
+    populate_news_api_task = PythonOperator(
+        task_id="populate_news_api",
+        python_callable=get_data_news_api,
+    )
+
+    test_postgres_connection_operator >> populate_news_api_task
+
+_get_data_news_api()    
