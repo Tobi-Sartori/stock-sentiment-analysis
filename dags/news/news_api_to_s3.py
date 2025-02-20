08de331c9664727bb1e10291b1fce114a8a13923
Tobias Sartori <tobiasfaculdade@gmail.com>
2025-02-20 02:35:48 -0300

    fixing dagid

---
+++ dags/news/news_api_to_s3.py
@@ -0,0 +1,62 @@
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from airflow.utils.task_group import TaskGroup
+import logging
+
+
+from include.python_operators.commons import (extract_keywords)
+
+
+from include.python_operators.news.news_api_to_s3 import (
+    get_news_data_to_s3,
+    scrap_url_content_save_s3,
+)
+
+default_args = {
+    "id": "news_api_to_s3",
+    "owner": "Tobias Sartori",
+    "start_date": days_ago(1),
+    "depends_on_past": True,
+    "retries": 1,
+    "max_active_runs": 1,
+}
+
+with DAG(
+    "news_api_to_s3_dev_test",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+    
+    start_task = DummyOperator(task_id="start_task")
+
+    extract_keywords_task = PythonOperator(
+        task_id="extract_keywords",
+        python_callable=extract_keywords,
+        op_args=["config_yamls/stocks.yaml"],
+    )
+
+    keywords = extract_keywords("config_yamls/stocks.yaml")
+
+    with TaskGroup("news_keyword_extract_load") as news_keyword_extract_load:
+        for keyword in keywords:
+            keyword_naming = keyword.replace(" ", "_")
+            get_news_task = PythonOperator( ## Get news data from news api, save json to s3
+                task_id=f"get_news_{keyword_naming}",
+                python_callable=get_news_data_to_s3,
+                op_kwargs={'keyword': keyword, 'yesterday': '{{ macros.ds_add(ds, -1) }}'},
+            )
+            
+            scrap_url_content_saveS3_task = PythonOperator( ## Read json from s3, Scrap url content from s3, save to s3
+                task_id=f"scrap_url_content_save_s3_{keyword_naming}",
+                python_callable=scrap_url_content_save_s3,
+                op_kwargs={'keyword': keyword, 'yesterday': '{{ macros.ds_add(ds, -1) }}'}
+            )
+
+            get_news_task >> scrap_url_content_saveS3_task
+
+    end_task = DummyOperator(task_id="end_task")
+
+    start_task >> extract_keywords_task >> news_keyword_extract_load >> end_task
